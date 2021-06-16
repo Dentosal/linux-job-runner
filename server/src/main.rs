d77de078fc31c2261d225d83135a7ef8d4f8bc39
Hannes Karppila <hannes.karppila@gmail.com>
2021-06-16 23:45:46 +0300

    Allow configuring server mTLS certificates

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,9 @@
 #![deny(unused_must_use)]
 
 use std::net::SocketAddr;
+use std::path::PathBuf;
+
+use clap::{AppSettings, Clap};
 
 use common::t_service_server::TServiceServer;
 use tonic::transport::{Certificate, Identity, Server, ServerTlsConfig};
@@ -12,35 +15,59 @@ mod service;
 
 use self::service::TServiceImpl;
 
+#[derive(Clap)]
+#[clap(version, author)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(
+        long = "client-root-ca-crt",
+        env = "TLS_CLIENT_ROOT_CA_CRT",
+        default_value = "certs/ca_client/root-ca.crt"
+    )]
+    client_root_ca_crt: PathBuf,
+    #[clap(
+        short = 'c',
+        long = "crt",
+        env = "TLS_SERVER_CRT",
+        default_value = "certs/server/server.crt"
+    )]
+    server_crt: PathBuf,
+    #[clap(
+        short = 'k',
+        long = "key",
+        env = "TLS_SERVER_KEY",
+        default_value = "certs/server/server.key"
+    )]
+    server_key: PathBuf,
+    /// The address to serve at
+    #[clap(default_value = "127.0.0.1:8000")]
+    bind: SocketAddr,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
-    // TODO: do argument parsing properly, e.g. with clap
-    let addr: SocketAddr = if let Some(arg) = std::env::args().nth(1) {
-        arg.parse().expect("Invalid bind address")
-    } else {
-        "127.0.0.1:8000".parse().unwrap()
-    };
+    let opts: Opts = Opts::parse();
 
-    // TODO: read certificates from argument paths, and not hard coded locations
-    let cert = tokio::fs::read("certs/server/server.crt").await?;
-    let key = tokio::fs::read("certs/server/server.key").await?;
-    let server_identity = Identity::from_pem(cert, key);
-    let client_ca_cert = tokio::fs::read("certs/ca_client/root-ca.crt").await?;
-    let client_ca_cert = Certificate::from_pem(client_ca_cert);
+    // TODO: better error messages
+    let crt = tokio::fs::read(opts.server_crt).await?;
+    let key = tokio::fs::read(opts.server_key).await?;
+    let server_identity = Identity::from_pem(crt, key);
+    let client_ca_crt = tokio::fs::read(opts.client_root_ca_crt).await?;
+    let client_ca_crt = Certificate::from_pem(client_ca_crt);
 
     let tls = ServerTlsConfig::new()
         .identity(server_identity)
-        .client_ca_root(client_ca_cert);
+        .client_ca_root(client_ca_crt);
 
     let service = TServiceImpl::new();
     let server = Server::builder()
         .tls_config(tls)?
         .add_service(TServiceServer::new(service));
 
-    log::info!("Serving at {}", addr);
-    server.serve(addr).await?;
+    log::info!("Serving at {}", opts.bind);
+    server.serve(opts.bind).await?;
 
     Ok(())
 }
