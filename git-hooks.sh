665f2745c4752ddc47a6ad16e3b520b3fba01b9f
Hannes Karppila <hannes.karppila@gmail.com>
2021-06-16 15:37:26 +0300

    Add a simple git hook manager script

---
+++ git-hooks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# Installs git hooks for cargo commands
+
+case "$1" in
+    install)
+        echo './git-hooks.sh pre-commit' > .git/hooks/pre-commit
+        echo './git-hooks.sh pre-push' > .git/hooks/pre-push
+
+        chmod +x .git/hooks/pre-commit
+        chmod +x .git/hooks/pre-push
+
+        ;;
+
+    pre-commit)
+        echo "Running format check"
+        cargo fmt -- --check
+        echo "Running clippy"
+        cargo clippy
+
+        ;;
+
+    pre-push)
+        $0 pre-commit
+        echo "Running tests"
+        cargo test
+
+        ;;
+    *)
+        echo "Use $0 install to install git hooks"
+        exit 1
+esac
